cba96ab0d0188cf50d6fff5f7bc7b0c63052e042
mmabadal <34652305+mmabadal@users.noreply.github.com>
2020-10-27 23:57:00 +0100

    towards our data input

--- utils/data_prepare_s3dis.py
+++ utils/data_prepare_s3dis.py
@@ -3,7 +3,10 @@ from os.path import join, exists, dirname, abspath
 import numpy as np
 import pandas as pd
 import os, sys, glob, pickle
-
+import os
+import sys
+import argparse
+from natsort import natsorted
 BASE_DIR = dirname(abspath(__file__))
 ROOT_DIR = dirname(BASE_DIR)
 sys.path.append(BASE_DIR)
@@ -11,35 +14,41 @@ sys.path.append(ROOT_DIR)
 from helper_ply import write_ply
 from helper_tool import DataProcessing as DP
 
-dataset_path = '/data/S3DIS/Stanford3dDataset_v1.2_Aligned_Version'
-anno_paths = [line.rstrip() for line in open(join(BASE_DIR, 'meta/anno_paths.txt'))]
-anno_paths = [join(dataset_path, p) for p in anno_paths]
+parser = argparse.ArgumentParser()
+parser.add_argument('--path_in', help='path to the txt data folder.')
+parser.add_argument('--path_out', help='path to save ply folder.')
+parser.add_argument('--path_cls', help='path to classes txt.')
 
-gt_class = [x.rstrip() for x in open(join(BASE_DIR, 'meta/class_names.txt'))]
+parsed_args = parser.parse_args(sys.argv[1:])
+
+path_in = parsed_args.path_in
+path_out = parsed_args.path_out
+path_cls = parsed_args.path_cls
+
+if not os.path.exists(path_out):
+    os.mkdir(path_out)
+
+gt_class = path_cls
 gt_class2label = {cls: i for i, cls in enumerate(gt_class)}
 
 sub_grid_size = 0.04
-original_pc_folder = join(dirname(dataset_path), 'original_ply')
-sub_pc_folder = join(dirname(dataset_path), 'input_{:.3f}'.format(sub_grid_size))
-os.mkdir(original_pc_folder) if not exists(original_pc_folder) else None
-os.mkdir(sub_pc_folder) if not exists(sub_pc_folder) else None
-out_format = '.ply'
 
-
-def convert_pc2ply(anno_path, save_path):
+def convert_pc2ply(case_path, save_path):
     """
     Convert original dataset files to ply file (each line is XYZRGBL).
     We aggregated all the points from each instance in the room.
-    :param anno_path: path to annotations. e.g. Area_1/office_2/Annotations/
+    :param case_path: path to case. e.g. Area_1/office_2/
     :param save_path: path to save original point clouds (each line is XYZRGBL)
     :return: None
     """
     data_list = []
 
+    anno_path = os.path.join(path_in, case_path, "annotations")
+
     for f in glob.glob(join(anno_path, '*.txt')):
         class_name = os.path.basename(f).split('_')[0]
         if class_name not in gt_class:  # note: in some room there is 'staris' class..
-            class_name = 'clutter'
+            print("ERROR, " + str(class_name) + " CLASE NAME NOT RECOGNIZED")
         pc = pd.read_csv(f, header=None, delim_whitespace=True).values
         labels = np.ones((pc.shape[0], 1)) * gt_class2label[class_name]
         data_list.append(np.concatenate([pc, labels], 1))  # Nx7
@@ -56,25 +65,22 @@ def convert_pc2ply(anno_path, save_path):
     # save sub_cloud and KDTree file
     sub_xyz, sub_colors, sub_labels = DP.grid_sub_sampling(xyz, colors, labels, sub_grid_size)
     sub_colors = sub_colors / 255.0
-    sub_ply_file = join(sub_pc_folder, save_path.split('/')[-1][:-4] + '.ply')
+    sub_ply_file = os.path.join(path_out, folder + ".ply" )
     write_ply(sub_ply_file, [sub_xyz, sub_colors, sub_labels], ['x', 'y', 'z', 'red', 'green', 'blue', 'class'])
 
     search_tree = KDTree(sub_xyz)
-    kd_tree_file = join(sub_pc_folder, str(save_path.split('/')[-1][:-4]) + '_KDTree.pkl')
+    kd_tree_file = os.path.join(path_out, folder + "_KDTree.pkl" )
     with open(kd_tree_file, 'wb') as f:
         pickle.dump(search_tree, f)
 
     proj_idx = np.squeeze(search_tree.query(xyz, return_distance=False))
     proj_idx = proj_idx.astype(np.int32)
-    proj_save = join(sub_pc_folder, str(save_path.split('/')[-1][:-4]) + '_proj.pkl')
+    proj_save = os.path.join(path_out, folder + "_proj.pkl" )
     with open(proj_save, 'wb') as f:
         pickle.dump([proj_idx, labels], f)
 
 
 if __name__ == '__main__':
-    # Note: there is an extra character in the v1.2 data in Area_5/hallway_6. It's fixed manually.
-    for annotation_path in anno_paths:
-        print(annotation_path)
-        elements = str(annotation_path).split('/')
-        out_file_name = elements[-3] + '_' + elements[-2] + out_format
-        convert_pc2ply(annotation_path, join(original_pc_folder, out_file_name))
+    for folder in natsorted(os.listdir(path_in)):
+        print("working on case: " + str(folder))
+        convert_pc2ply(folder, path_out)
