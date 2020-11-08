81b6534a7bc6886d975097075fc0bf1c9e0adafc
mmabadal <mmabadal@gmail.com>
2020-11-08 11:44:43 +0100

    change output, labels as colors (chapuza)

--- helper_ply.py
+++ helper_ply.py
@@ -292,10 +292,17 @@ def write_ply(filename, field_list, field_names, triangular_faces=None):
         # End of header
         header.append('end_header')
 
+        for i in range(field_list[0].shape[0]):
+            preds = field_list[0]
+            pred = int(preds[i])*20
+            header.append(str(pred))
+1
+
         # Write all lines
         for line in header:
             plyfile.write("%s\n" % line)
 
+    '''
     # open in binary/append to use tofile
     with open(filename, 'ab') as plyfile:
 
@@ -324,7 +331,8 @@ def write_ply(filename, field_list, field_names, triangular_faces=None):
             data['1'] = triangular_faces[:, 1]
             data['2'] = triangular_faces[:, 2]
             data.tofile(plyfile)
-
+    '''
+    
     return True
 
 
